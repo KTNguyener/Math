65baa45d1e1dcb367f73b8cc8007b9c6d785b55f
KTNguyener <30875266+KTNguyener@users.noreply.github.com>
2017-08-28 08:44:59 -1000

    Added practice file for updating objects

---
+++ updated.js
@@ -0,0 +1,11 @@
+var newName = prompt("what is Cole's new first name?")
+
+var cole = {
+	fName:"Cole",
+	lName: "House",
+	age: 15,
+	grade: 10,
+}
+
+window.alert(cole.fName = newName);
+window.alert(cole.fName)
