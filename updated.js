add93ddd4bd6c2efde6065f9a4a2e3b097cd6aac
KTNguyener <30875266+KTNguyener@users.noreply.github.com>
2017-08-28 08:48:38 -1000

    Update updated.js

--- updated.js
+++ updated.js
@@ -1,13 +1,25 @@
 var newName = prompt("what is Cole's new first name?")
-
+//takes user's prompt
 var cole = {
 	fName:"Cole",
 	lName: "House",
 	age: 15,
 	grade: 10,
 }
-
+//cole's info
 window.alert(cole.fName = newName);
+//window alert that takes user's imput and replaces cole's firstname
 window.alert(cole.fName)
-
+//test to ensure that cole's first name has been changed
 //August 28, replacing values in a object literal
+
+var x = prompt("enter fName, lName, age, or grade")
+
+var students = {
+	fName:"Kiva",
+	lName: "Nguyen",
+	age: 17,
+	grade: 12,
+}
+
+window.alert(students[x]);
