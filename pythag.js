e032191d054f312393fbf4801610d270235ebf6f
KTNguyener <30875266+KTNguyener@users.noreply.github.com>
2017-09-13 09:02:07 -1000

    Added a pythagorean calculator

---
+++ pythag.js
@@ -0,0 +1,7 @@
+function calculate() {
+  var x=document.getElementById('a').value;
+  var y=document.getElementById('b').value;
+  var z= Math.sqrt((Math.pow(x,2))+(Math.pow(y,2)))
+ 
+  document.getElementById('c').innerHTML="C= " + z;
+}
